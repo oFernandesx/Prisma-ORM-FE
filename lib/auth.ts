8f4a32d477295f1a5ebfc401ec6d60fe417f26dd
Fernandes <fernandesocaio@gmail.com>
2025-10-29 19:51:14 -0300

    chore: finish sign-up and sign-in

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,19 +1,12 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "sqlite",
-    }),
-    emailAndPassword: {
-        enabled: true,
-    },
-
-    plugins: [nextCookies()]
-
+  adapter: prismaAdapter(prisma, { provider: "sqlite" }),
+  emailAndPassword: { enabled: true },
+  plugins: [nextCookies()],
 });
